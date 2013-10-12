c36db3e0f630084bac2dbdaad7db9552938f47a1
Siva Cn <cnsiva.in@gmail.com>
2013-10-13 01:02:30 +0530

    Updated (Stable Verson)

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 Crawler
 =======
 
-A File based URL Crawler
+Introduction:
+
+    This is a Website URL crawler program, for given an
+    url it start searching the url's that are present in
+    the specified url, and so on.. until the limit is
+    acheived.
+
+Usage:
+
+    python<ver> Crawler.py -u <url> -l <limit-in-number>
+
+dependencies:
+
+    Third party modules:
+        1) Beautifulsoup
+
--- src/Crawler.py
+++ src/Crawler.py
@@ -29,31 +29,36 @@ from linked_list import LinkedList
 class Crawler(ArgParse, LinkedList):
     def __init__(self):
         """."""
-        self.count_parsed_url = 0
+        options = ArgParse().parse()
+
+        self.start_url = options.url
+        self.limit = int(options.limit)
+        self.processed_url = []
 
     def __call__(self):
         """."""
-        options = ArgParse().parse()
         grabber_obj = Grabber()
 
-        url_list = []
+        linked_list_obj = LinkedList(self.start_url)
 
-        ll_obj = LinkedList(options.url)
+        _url_list = grabber_obj(self.start_url)
 
-        ## Start Looping here...
-        if self.count_parsed_url >= options.limit:
-            pass
+        linked_list_obj.insert(_url_list)
+        
+        walker = linked_list_obj.list.start
 
-        _url_list = grabber_obj(options.url)
+        while walker:
+            if linked_list_obj.list.count <= self.limit:
+                _url_list = grabber_obj(options.url)
+                _url_list = [ele for ele in _url_list \
+                             if ele not in self.processed_url]
 
-        ll_obj.insert(_url_list)
+                linked_list_obj.insert(_url_list)
 
-        ll_obj.show_list()
+            walker = walker.next
 
-        url_list.extend(_url_list)
+        linked_list_obj.show_list(self.limit)
 
-        self.count_parsed_url += len(url_list)
-        print self.count_parsed_url
 
 if __name__ == "__main__":
     """First Block of statement."""

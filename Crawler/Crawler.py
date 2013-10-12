2ecaf7cb2436d9886430627d55e36c68ffd7dec6
Siva Cn <cnsiva.in@gmail.com>
2013-10-12 20:32:41 +0530

    Created class Crawler

---
+++ Crawler/Crawler.py
@@ -0,0 +1,34 @@
+#! /usr/bin/python
+
+##############################################################################
+##
+##  Copyright GPL (www.cnsiva.com)
+##
+##############################################################################
+
+"""A File Based Url Crawler.
+"""
+
+__all__ = ['Crawler']
+
+__author__ = r"SivaCn (http://www.cnsiva.com)"
+__email__ = r"cnsiva.in@gmail.com"
+
+## ------ Built-in Imports ------ ##
+## ------ Built-in Imports ------ ##
+
+## ------ Local Imports ------ ##
+## ------ Local Imports ------ ##
+
+
+class Crawler(object):
+    """."""
+    def __init__(self):
+        """."""
+        pass
+
+
+if __name__ == "__main__":
+    """First Block of statement."""
+    obj = Crawler()
+

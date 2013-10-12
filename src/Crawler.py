bf457f62273b893a95383f83b52f86c6fa478b4f
Siva Cn <cnsiva.in@gmail.com>
2013-10-12 22:07:49 +0530

    removing Crawler dir

---
+++ src/Crawler.py
@@ -0,0 +1,37 @@
+#! /usr/bin/python
+
+###############################################################################
+##
+##
+##
+###############################################################################
+
+"""
+
+"""
+
+__all__ = ['Crawler']
+
+__author__ = r"SivaCn (http://www.cnsiva.com)"
+__email__ = r"cnsiva.in@gmail.com"
+
+## ------- Built-in Imports ------ ##
+from defaults import *
+## ------- Built-in Imports ------ ##
+
+## ------- Package Imports ------ ##
+## ------- Package Imports ------ ##
+
+
+class Crawler():
+    def __init__(self):
+        """."""
+        pass
+
+    def __call__(self):
+        """."""
+        pass
+
+if __name__ == "__main__":
+    """First Block of statement."""
+    Crawler()()

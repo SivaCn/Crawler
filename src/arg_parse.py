d0e03b7c18ac79fa51b8b0d636768c33638f2c29
Siva Cn <cnsiva.in@gmail.com>
2013-10-12 22:03:26 +0530

    modified

---
+++ src/arg_parse.py
@@ -0,0 +1,27 @@
+#! /usr/bin/python
+
+###############################################################################
+##
+##
+##
+###############################################################################
+
+"""
+
+"""
+
+__all__ = []
+
+__author__ = r"SivaCn (http://www.cnsiva.com)"
+__email__ = r"cnsiva.in@gmail.com"
+
+## ------- Built-in Imports ------ ##
+## ------- Built-in Imports ------ ##
+
+## ------- Package Imports ------ ##
+## ------- Package Imports ------ ##
+
+
+if __name__ == "__main__":
+    """First Block of statement."""
+    pass
---
+++ src/defaults.py
@@ -0,0 +1,31 @@
+#! /usr/bin/python
+
+###############################################################################
+##
+##
+##
+###############################################################################
+
+"""
+
+"""
+
+__all__ = []
+
+__author__ = r"SivaCn (http://www.cnsiva.com)"
+__email__ = r"cnsiva.in@gmail.com"
+
+## ------- Built-in Imports ------ ##
+import re
+import os
+import sys
+import cPickle
+## ------- Built-in Imports ------ ##
+
+## ------- Package Imports ------ ##
+## ------- Package Imports ------ ##
+
+
+if __name__ == "__main__":
+    """First Block of statement."""
+    pass
